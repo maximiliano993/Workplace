579d2e83c4121ed041b7f210f64dbb4f2963f7d9
maximiliano993 <68905000+maximiliano993@users.noreply.github.com>
2020-08-15 20:31:41 -0300

    1.2

--- js/init.js
+++ js/init.js
@@ -7,6 +7,8 @@ const PRODUCT_INFO_COMMENTS_URL = "https://japdevdep.github.io/ecommerce-api/pro
 const CART_INFO_URL = "https://japdevdep.github.io/ecommerce-api/cart/987.json";
 const CART_BUY_URL = "https://japdevdep.github.io/ecommerce-api/cart/buy.json";
 
+
+
 var showSpinner = function(){
   document.getElementById("spinner-wrapper").style.display = "block";
 }
@@ -39,6 +41,8 @@ var getJSONData = function(url){
         return result;
     });
 }
+
+
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
--- js/products.js
+++ js/products.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function (e) {
         let container=document.getElementsByClassName('container p-5')[0]
         for (i=0; i<json.data.length; i++){
             let product_info='<div class="product">';
-            product_info+='<ul class="product-data">';
+            product_info+='<ul class="list-no-style">';
             product_info+='<li>' + json.data[i]['name'] + " " + json.data[i]['cost'] + json.data[i]['currency'] + '</li>';
             product_info+='<li><img src=' + json.data[i]['imgSrc'] + ' alt="imagen" class="product-image"></li>';
             product_info+='<li class="product-description">' + json.data[i]['description'] + '</li>'
