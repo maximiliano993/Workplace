608eb57ee83c71cd7b694dc18cbcae45b9a1303c
maximiliano993 <68905000+maximiliano993@users.noreply.github.com>
2020-08-15 23:54:20 -0300

    1.3

--- js/products.js
+++ js/products.js
@@ -3,16 +3,15 @@
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
     getJSONData(PRODUCTS_URL).then(function(json){
-        let container=document.getElementsByClassName('container p-5')[0]
         for (i=0; i<json.data.length; i++){
             let product_info='<div class="product">';
             product_info+='<ul class="list-no-style">';
-            product_info+='<li>' + json.data[i]['name'] + " " + json.data[i]['cost'] + json.data[i]['currency'] + '</li>';
-            product_info+='<li><img src=' + json.data[i]['imgSrc'] + ' alt="imagen" class="product-image"></li>';
-            product_info+='<li class="product-description">' + json.data[i]['description'] + '</li>'
+            product_info+='<li ><h4 class="center"><strong>' + json.data[i]['name'] + " " + json.data[i]['cost'] + json.data[i]['currency'] + '</strong></h4></li>';
+            product_info+='<li><img src=' + json.data[i]['imgSrc'] + ' alt="imagen" class="product-size"></li>';
+            product_info+='<li class="product-size">' + json.data[i]['description'] + '</li>'
             product_info+='</ul>';
             product_info+='</div>';
-            container.innerHTML+=product_info;
+            document.getElementsByClassName('container-product')[0].innerHTML+=product_info;
         }
     })
 })
